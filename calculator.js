4759ddc8caa9b5f45cb6992bc173c46b10b169f6
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-07-11 16:09:35 -0500

    Populate fields.

--- calculator.js
+++ calculator.js
@@ -1,8 +1,48 @@
 function calculate() {
+
     var pellet_diameter_mm = parseFloat(document.getElementById("pellet_diameter_mm").value);
     var pellet_radius_mm = pellet_diameter_mm/2;
     var pellet_area = Math.PI * pellet_radius_mm * pellet_radius_mm;
     document.getElementById("pellet_area").value = pellet_area;
+
+    var sample_absorption_length_um = parseFloat(document.getElementById("sample_absorption_length_um").value);
+    var desired_absorption_lengths = parseFloat(document.getElementById("desired_absorption_lengths").value);
+    const um_to_mm = 1000.0;
+    var sample_volume = desired_absorption_lengths * (sample_absorption_length_um/um_to_mm) * pellet_area;
+    document.getElementById("sample_volume").value = sample_volume;
+
+    var sample_thick_mm = sample_volume / pellet_area;
+    document.getElementById("sample_thick_mm").value = sample_thick_mm;
+
+    var binder_volume_ratio = parseFloat(document.getElementById("binder_volume_ratio").value);
+    var binder_volume = binder_volume_ratio * sample_volume;
+    document.getElementById("binder_volume").value = binder_volume;
+
+    var binder_thick_mm = binder_volume / pellet_area;
+    document.getElementById("binder_thick_mm").value = binder_thick_mm;
+
+    var filler_volume_ratio = parseFloat(document.getElementById("filler_volume_ratio").value);
+    var filler_volume = filler_volume_ratio * sample_volume;
+    document.getElementById("filler_volume").value = filler_volume;
+
+    var filler_thick_mm = filler_volume / pellet_area;
+    document.getElementById("filler_thick_mm").value = filler_thick_mm;
+
+    var total_thick_mm = sample_thick_mm + binder_thick_mm + filler_thick_mm;
+    document.getElementById("total_thick_mm").value = total_thick_mm;
+
+    var sample_density = parseFloat(document.getElementById("sample_density").value);
+    var sample_mass_mg = sample_volume * sample_density;
+    document.getElementById("sample_mass_mg").value = sample_mass_mg;
+
+    var binder_density = parseFloat(document.getElementById("binder_density").value);
+    var binder_mass_mg = binder_volume * binder_density;
+    document.getElementById("binder_mass_mg").value = binder_mass_mg;
+
+    var filler_density = parseFloat(document.getElementById("filler_density").value);
+    var filler_mass_mg = filler_volume * filler_density;
+    document.getElementById("filler_mass_mg").value = filler_mass_mg;
+
 }
 
 function table_to_csv(table) {
